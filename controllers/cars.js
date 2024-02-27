0c9bcc456c612d7deb7c30be20dc0ea8838a4b6c
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-02-27 19:22:39 +0700

    feat: implement database operations for car route

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,22 +1,60 @@
-exports.getCars = (req, res, next) => {
-    res.status(200).json({success:true});
+const Car = require('../models/car');
+
+exports.getCars = async (req, res, next) => {
+    try{
+        const cars = await Car.find();
+        res.status(200).json({success: true, data: cars});
+    }
+    catch(err){
+        res.status(400).json({success: false});
+    }
 }
 
-exports.createCar = (req, res, next) => {
-    res.status(200).json({success:true});
+exports.createCar = async (req, res, next) => {
+    const car = await Car.create(req.body);
+    res.status(201).json({success: true, data: car});
 
 }
 
-exports.getCar = (req, res, next) => {
-    res.status(200).json({success:true});
+exports.getCar = async (req, res, next) => {
+    try{
+        const car = await Car.findById(req.params.id);
+        if(!car){
+            return res.status(400).json({success: false});
+        }
+        res.status(200).json({success: true, data: car});
+    }
+    catch(err){
+        res.status(400).json({success: false});
+    }
+}
+
+exports.updateCar = async (req, res, next) => {
+    try{
+        const car = await Car.findByIdAndUpdate(req.params.id,req.body,{
+        new: true,
+        runValidators: true
+        });
+        if(!car){
+            return res.status(400).json({success: false});
+        }
+        res.status(200).json({success: true, data:car});
+    }
+    catch(err){
+        res.status(400).json({success: false});
+    }
 
 }
 
-exports.updateCar = (req, res, next) => {
-    res.status(200).json({success:true});
-
-}
-
-exports.deleteCar = (req, res, next) => {
-    res.status(200).json({success:true});
+exports.deleteCar = async (req, res, next) => {
+    try{
+        const car = await Car.findByIdAndDelete(req.params.id);
+        if(!car){
+            return res.status(400).json({success: false});
+        }
+        res.status(200).json({success: true, data: car});
+    }
+    catch(err){
+        res.status(400).json({success: false});
+    }
 }
