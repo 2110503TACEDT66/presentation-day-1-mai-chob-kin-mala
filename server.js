3e0bed1877c4b6ac4ea25fda8db7870c8ec979e3
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-02-27 17:45:28 +0700

    feat: add connection with mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 const app = express();
 dotenv.config({path:'./config/config.env'});
+connectDB();
 
 const cars = require('./routes/cars');
 
