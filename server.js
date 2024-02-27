09ccc06fa1bf8c0b119ac2f71a68e833026f7db1
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-02-27 17:57:34 +0700

    fix: add Unhandled Rejection handle

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use('/api/v1/cars',cars);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log('Server running in',process.env.NODE_ENV,'mode on PORT',PORT);
+});
+
+process.on('unhandledRejection',(err,promise) => {
+    console.log('ERR:',err.message);
+    server.close(()=>{process.exit(1)});
 });
