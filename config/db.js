c4e3a7f65d99823dad342e63784dcbd1de398ffe
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-02 03:59:45 +0700

    fix: remove space in front of ':'

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async ()=>{
     mongoose.set('strictQuery',true);
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Connected :',conn.connection.host);
+    console.log('MongoDB Connected:',conn.connection.host);
 }
 
 module.exports = connectDB;
