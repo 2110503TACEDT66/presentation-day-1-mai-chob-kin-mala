c2abcafa370a18c1d57259839704ea1fa4b0b714
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-03 05:44:18 +0700

    fix: error messages

--- models/Car.js
+++ models/Car.js
@@ -3,33 +3,33 @@ const mongoose = require("mongoose");
 const CarSchema = mongoose.Schema({
     license: {
         type: String,
-        required: [true, "Please enter car's license"],
+        required: [true, "Please add car's license"],
         unique: true,
         maxLength: [10, "License can not be more than 10 characters"],
     },
     type: {
         type: String,
-        required: [true, "Please enter car's type"],
+        required: [true, "Please add car's type"],
         maxLength: [30, "Type can not be more than 30 characters"],
     },
     model: {
         type: String,
-        required: [true, "Please enter car's model"],
+        required: [true, "Please add car's model"],
         maxlength: [30, "Type can not be more than 30 characters"],
     },
     color: {
         type: [String],
-        required: [true, "Please enter car's color"],
+        required: [true, "Please add car's color"],
         maxlength: [20, "Color can not be more than 20 characters"],
     },
     fuel_type: {
         type: String,
-        required: [true, "Please enter car's fuel type"],
+        required: [true, "Please add car's fuel type"],
         maxlength: [20, "Fuel type can not be more than 20 characters"],
     },
     year: {
         type: Number,
-        required: [true, "Please enter car's year"],
+        required: [true, "Please add car's year"],
     },
     price: {
         type: Number,
@@ -37,20 +37,20 @@ const CarSchema = mongoose.Schema({
     },
     mileage: {
         type: Number,
-        required: [true, "Please enter car's mileage"],
+        required: [true, "Please add car's mileage"],
     },
     condition: {
         type: String,
-        required: [true, "Please enter car's condition"],
+        required: [true, "Please add car's condition"],
         maxlength: [20, "Condition can not be more than 20 characters"],
     },
     image: {
         type: [String],
-        required: [true, "Please enter car's image"],
+        required: [true, "Please add car's image"],
     },
     number_of_seats: {
         type: Number,
-        required: [true, "Please enter car's number_of_seats"],
+        required: [true, "Please add car's number_of_seats"],
     },
 },{
     toJSON: {virtuals: true},
