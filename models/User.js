37e657f2493c4bf73451fb611a984988c41a40a8
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-03 06:20:23 +0700

    fix: add next() in pre save

--- models/User.js
+++ models/User.js
@@ -53,6 +53,7 @@ const UserSchema = mongoose.Schema({
 UserSchema.pre("save", async function (next) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 UserSchema.methods.getSignedJwtToken = function (enteredPassword) {
