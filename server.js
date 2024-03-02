e2ba5614134babc9d585515cd40cd9a1b0c05900
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-02 12:18:25 +0700

    style: format server.js code

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 
-const app = express();
-dotenv.config({path:'./config/config.env'});
-connectDB();
-
 const cars = require('./routes/cars');
 const auth = require('./routes/auth');
+
+const app = express();
+dotenv.config({path:'./config/config.env'});
+
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/v1/cars',cars);
 app.use('/api/v1/auth',auth);
-
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
