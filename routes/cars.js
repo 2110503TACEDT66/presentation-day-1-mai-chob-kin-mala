139aeac3b041d911351062cb5c964f333935ae17
Siratee Topradit <tae.siratee@gmail.com>
2024-03-02 15:04:11 +0700

    Update cars.js

--- routes/cars.js
+++ routes/cars.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {getCars, createCar, getCar, updateCar, deleteCar} = require('../controllers/cars');
+const {
+  getCars,
+  createCar,
+  getCar,
+  updateCar,
+  deleteCar,
+} = require("../controllers/cars");
 
-router.route('/')
-    .get(getCars)
-    .post(createCar);
-router.route('/:id')
-    .get(getCar)
-    .put(updateCar)
-    .delete(deleteCar);
+router.route("/").get(getCars).post(createCar);
+router.route("/:id").get(getCar).put(updateCar).delete(deleteCar);
 
 module.exports = router;
-
