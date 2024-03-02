da4726db0aa2c410af5823e840cb7c88f928e580
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-02 16:54:16 +0700

    feat: protect route /me

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,6 @@ const { protect } = require("../middleware/auth");
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/me").get(getMe);
+router.route("/me").get(protect, getMe);
 
 module.exports = router;
