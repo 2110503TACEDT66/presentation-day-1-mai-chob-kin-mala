36f77a980baf88f55cea07629e724751cdc2446d
Caelus-J <namex648@gmail.com>
2024-03-04 16:46:37 +0700

    edit ShopSchema.pre

--- models/Shop.js
+++ models/Shop.js
@@ -38,7 +38,8 @@ ShopSchema.virtual('cars', {
 
 ShopSchema.pre('deleteOne',{document: true, query: false}, async function(next){
     console.log(`Cars being removed from shop ${this._id}`);
-    await Car.deleteMany({shop: this._id});
+    const car = await Car.find({shop: this._id});
+    await Promise.all(car.map(car => car.deleteOne()));
     
     next();
 });
