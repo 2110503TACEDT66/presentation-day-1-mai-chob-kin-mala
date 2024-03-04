52157b2201112630affbc66ee1858fd7681f02c8
Caelus-J <namex648@gmail.com>
2024-03-04 15:11:38 +0700

    rename routes/shop.js --> routes/shops.js and add getShops, getShop, createShop, updateShop, deleleShop

--- routes/shops.js
+++ routes/shops.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { protect, authorize } = require("../middleware/auth");
-const { getShop, getShops, createShop, updateShop, deleteShop } = require('../controllers/rentalcars');
+const { getShop, getShops, createShop, updateShop, deleteShop } = require('../controllers/shops');
 
 router.route('/').get(protect, authorize('admin'), getShops).post(protect, authorize('admin'), createShop);
 router.route('/:id').get(protect, authorize('admin'), getShop).put(protect, authorize('admin'), updateShop).delete(protect, authorize('admin'), deleteShop);
