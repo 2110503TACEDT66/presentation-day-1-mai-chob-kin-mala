a8b31a9c9861eade4327e3e37eb5151794a4e68d
Siratee Topradit <tae.siratee@gmail.com>
2024-03-25 18:41:11 +0700

    Update server port and log message

--- server.js
+++ server.js
@@ -38,13 +38,14 @@ app.use("/api/v1/auth", auth);
 app.use("/api/v1/bookings", bookings);
 app.use("/api/v1/shops", shops);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const server = app.listen(PORT, () => {
     console.log(
         "Server running in",
-        process.env.NODE_ENV,
-        "mode on PORT",
-        PORT
+        process.env.HOST+
+        ":"+
+        PORT+
+        "/api/v1/"
     );
 });
 
