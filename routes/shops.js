a039531df848f25595b71780c4e3d9871eba8852
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-25 22:51:17 +0700

    feat: remove protect getShops & getShop

--- routes/shops.js
+++ routes/shops.js
@@ -4,8 +4,8 @@ const { protect, authorize } = require("../middleware/auth");
 const { getShop, getShops, createShop, updateShop, deleteShop } = require('../controllers/shops');
 const carRouter = require('./cars');
 
-router.route('/').get(protect, authorize('admin'), getShops).post(protect, authorize('admin'), createShop);
-router.route('/:id').get(protect, authorize('admin'), getShop).put(protect, authorize('admin'), updateShop).delete(protect, authorize('admin'), deleteShop);
+router.route('/').get(getShops).post(protect, authorize('admin'), createShop);
+router.route('/:id').get(getShop).put(protect, authorize('admin'), updateShop).delete(protect, authorize('admin'), deleteShop);
 router.use('/:shopId/cars', carRouter);
 
 module.exports = router;
